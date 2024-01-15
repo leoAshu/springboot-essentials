5b12fe27a2d1906e35835a7b30be83ae405ad119
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-14 21:57:37 -0800

    properly installs derby dependency

--- quick-start/rest-api/src/main/java/com/leo/ashu/restapi/RestApiApplication.java
+++ quick-start/rest-api/src/main/java/com/leo/ashu/restapi/RestApiApplication.java
@@ -2,9 +2,8 @@ package com.leo.ashu.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
