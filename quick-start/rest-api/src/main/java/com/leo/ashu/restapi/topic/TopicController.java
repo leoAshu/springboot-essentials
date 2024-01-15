f713973585e5f6242dbbd3d05da746db6466c110
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-14 20:22:43 -0800

    creates all crud methods and service operations for topic

--- quick-start/rest-api/src/main/java/com/leo/ashu/restapi/topic/TopicController.java
+++ quick-start/rest-api/src/main/java/com/leo/ashu/restapi/topic/TopicController.java
@@ -1,10 +1,14 @@
 package com.leo.ashu.restapi.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +22,23 @@ public class TopicController {
         return topicService.getAllTopics();
     }
 
-    // GET      /topics/id
-    // POST     /topics
-    // PUT      /topics/id
-    // DELETE   /topics/id
+    @GetMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @PostMapping("/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @PutMapping("/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @DeleteMapping("/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
 }
